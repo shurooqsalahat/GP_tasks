e0dcfef4a1f63996ae11ee4c6d6b55c7ba14f0e7
yaraBB <yara.a.barhoush@gmail.com>
2019-05-17 23:50:14 +0300

    buttons

--- supervisor/live_select_task.php
+++ supervisor/live_select_task.php
@@ -30,10 +30,10 @@ for ($i = 0; $i < $nor; $i++) {
         '<td>'.$row[4].'</td>'.
          '<td>'.$row[1].'</td>'.
          '<td>'.
-            '<button style="margin-right: 6px;" data-toggle="modal" data-target="#update_task_modal" type="button" class="btn btn-primary btn-sm dt-edit update_btn" id="update-modal-btn" data-id="'.$row[0].'">'.
+            '<button style="margin-right: 6px;border-radius: 15px;" data-toggle="modal" data-target="#update_task_modal" type="button" class="btn btn-primary btn-sm dt-edit update_btn" id="update-modal-btn" data-id="'.$row[0].'">'.
                 '<span class="glyphicon glyphicon-pencil " aria-hidden="true" ></span>'.
             '</button>'.
-            '<button type="button" class="btn btn-danger btn-sm dt-delete delete_btn" data-id3="'.$row[0].'">'.
+            '<button type="button" class="btn btn-danger btn-sm dt-delete delete_btn" atyle="border-radius: 15px;"data-id3="'.$row[0].'">'.
                 '<span class="glyphicon glyphicon-remove " aria-hidden="true" ></span>'.
              '</button>'.
         '</td>'.

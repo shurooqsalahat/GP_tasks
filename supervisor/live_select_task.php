ebd4459e0ec871d1655f4fd2f9cce9bfb8664040
yaraBB <yara.a.barhoush@gmail.com>
2019-05-18 09:18:19 +0300

    buttons

--- supervisor/live_select_task.php
+++ supervisor/live_select_task.php
@@ -33,7 +33,7 @@ for ($i = 0; $i < $nor; $i++) {
             '<button style="margin-right: 6px;border-radius: 15px;" data-toggle="modal" data-target="#update_task_modal" type="button" class="btn btn-primary btn-sm dt-edit update_btn" id="update-modal-btn" data-id="'.$row[0].'">'.
                 '<span class="glyphicon glyphicon-pencil " aria-hidden="true" ></span>'.
             '</button>'.
-            '<button type="button" class="btn btn-danger btn-sm dt-delete delete_btn" atyle="border-radius: 15px;"data-id3="'.$row[0].'">'.
+            '<button type="button" class="btn btn-danger btn-sm dt-delete delete_btn" style="border-radius: 15px;"data-id3="'.$row[0].'">'.
                 '<span class="glyphicon glyphicon-remove " aria-hidden="true" ></span>'.
              '</button>'.
         '</td>'.

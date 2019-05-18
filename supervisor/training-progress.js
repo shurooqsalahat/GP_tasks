cfb63594777af0afed0d562820d8b5f866168384
yaraBB <yara.a.barhoush@gmail.com>
2019-05-18 13:24:54 +0300

    training progress

--- supervisor/training-progress.js
+++ supervisor/training-progress.js
@@ -1,9 +1,6 @@
-var selectedItem = function () {
-    var selected = document.getElementById('search-type');
-    if (selected.value == 'student_name'|| selected.value == 'task_name') {
-        $('#NAME').show();
-        $('#ID').hide();
-        if(selected.value == 'student_name'){
+var selectedItem = function (selected) {
+    if (selected == 'student_name'|| selected == 'task_name') {
+        if(selected == 'student_name'){
             $('#assign-new-task-btn').show() ;
             $('#assign-new-student-btn').hide() ;
             $('#assign-tasks-table').show() ;
@@ -16,24 +13,97 @@ var selectedItem = function () {
             $('#assign-students-table').show() ;
             $('#assign-tasks-table').hide() ;
         }
+
+    }
+
+    else if (selected == 'student_id' || selected == 'task_id') {
+        $('#ID').show();
+        $('#NAME').hide();
+        if(selected == 'student_id'){
+            $('#assign-new-task-btn').show() ;
+            $('#assign-new-student-btn').hide() ;
+            $('#assign-tasks-table').show() ;
+            $('#assign-students-table').hide() ;
+        }
+        else{
+            $('#assign-new-task-btn').hide() ;
+            $('#assign-new-student-btn').show() ;
+            $('#assign-students-table').show() ;
+            $('#assign-tasks-table').hide() ;
+
+        }
+
+    }
+};
+
+var validate_search_btn = function () {
+     var Type = $('#search-type').val();
+     var name = $('#search_name').val();
+     var id = $('#search_id').val();
+
+     var vaidate = false;
+
+     $(".error").remove();
+
+     if (name.length < 1 && (Type =='student_name' ||Type=='task_name' )) {
+         $('#search_name').after('<div class="error">This field is required</div>');
+         vaidate =true;
+     }
+     if (id.length < 1 && (Type =='student_id' ||Type=='task_id' )) {
+         $('#search_id').after('<div class="error">This field is required</div>');
+         vaidate =true;
+     }
+     if (Type == '') {
+         $('#search-type').after('<div class="error">This field is required</div>');
+         vaidate =true;
+
+     }
+
+
+     if (!vaidate){
+        // document.getElementById("search_form").submit();
+         selectedItem(Type);
+     }
+ }
+
+var changeLabel = function () {
+    $('#assign-students-table').hide() ;
+    $('#assign-tasks-table').hide() ;
+    $('#assign-new-task-btn').hide() ;
+    $('#assign-new-student-btn').hide() ;
+    var selected = document.getElementById('search-type');
+    if (selected.value == 'student_name'|| selected.value == 'task_name') {
+        $('#NAME').show();
+        $('#ID').hide();
 
     }
     else if (selected.value == 'student_id' || selected.value == 'task_id') {
         $('#ID').show();
         $('#NAME').hide();
-        if(selected.value == 'student_id'){
-            $('#assign-new-task-btn').show() ;
-            $('#assign-new-student-btn').hide() ;
-            $('#assign-tasks-table').show() ;
-            $('#assign-students-table').hide() ;
-        }
-        else{
-            $('#assign-new-task-btn').hide() ;
-            $('#assign-new-student-btn').show() ;
-            $('#assign-students-table').show() ;
-            $('#assign-tasks-table').hide() ;
-
-        }
 
     }
-}
+};
+
+var atLeastChooseOneTask =function () {
+    var tasks = $('#assigned_tasks').val();
+    var validate=false;
+    if(tasks.length == 0){
+        $('#assigned_tasks').after('<div class="error">You have to chooose at least one task to assign it </div>');
+        validate =true;
+    }
+    if (!validate){
+        document.getElementById("add_new_task_for_student_form").submit();
+    }
+};
+
+var atLeastChooseOneStudent =function () {
+    var students = $('#students_to_assign_task').val();
+    var validate=false;
+    if(students.length == 0){
+        $('#students_to_assign_task').after('<div class="error">You have to chooose at least one student to assign this task . </div>');
+        validate =true;
+    }
+    if (!validate){
+        document.getElementById("add_new_task_for_student_form").submit();
+    }
+};
--- supervisor/training-progress.php
+++ supervisor/training-progress.php
@@ -98,40 +98,45 @@ session_start();
                 <?php if (isset($msg))
                     echo $msg; ?>
             </div>
-            <div class="form-group">
-                <label for="sel1">Search (select one):</label>
-                <select class="form-control" id="search-type" onchange="selectedItem()">
-                    <option value="">select</option>
-                    <option value="student_name">Student Name</option>
-                    <option value="student_id">Student Id</option>
-                    <option value="task_name">Task Name</option>
-                    <option value="task_id">Task id</option>
-                </select>
-            </div>
-            <div class="form-group" id="NAME">
-                <label for="usr">Name:</label>
-                <input type="text" class="form-control" id="usr">
-            </div>
-            <div class="form-group"  id="ID" style="display: none">
-                <label for="usr">ID:</label>
-                <input type="text" class="form-control" id="usr">
-            </div>
-            <div style="float: right">
-                <button class="button" style="vertical-align:middle"><span>Search</span></button>
-                <button class="button" style="vertical-align:middle;display: none" id="assign-new-task-btn" ><span>Assign new task</span></button>
-                <button class="button" style="vertical-align:middle;display: none" id="assign-new-student-btn"><span>Assign new student</span></button>
-
-            </div>
-
-            </div>
+            <form id="search_form">
+                <div class="form-group">
+                    <label for="search-type">Search (select one):</label>
+                    <select class="form-control" id="search-type" onchange="changeLabel()"
+                        <option value="">select</option>
+                        <option value="student_name">Student Name</option>
+                        <option value="student_id">Student Id</option>
+                        <option value="task_name">Task Name</option>
+                        <option value="task_id">Task id</option>
+                    </select>
+                </div>
+                <div class="form-group" id="NAME">
+                    <label for="search_name">Name:</label>
+                    <input type="text" class="form-control" id="search_name">
+                </div>
+                <div class="form-group"  id="ID" style="display: none">
+                    <label for="search_id">ID:</label>
+                    <input type="text" class="form-control"  id="search_id">
+                </div>
+                <div style="float: right">
+                    <button type="button" class="button" id="search-btn" onclick="validate_search_btn()"><span>Search</span></button>
+                    <button type="button" class="button" style="vertical-align:middle;display: none" id="assign-new-task-btn" data-toggle="modal" data-target="#assign_new_task_modal"><span>Assign new task</span></button>
+                    <button type="button" class="button" style="vertical-align:middle;display: none" id="assign-new-student-btn" data-toggle="modal" data-target="#assign_new_student_modal"><span>Assign new student</span></button>
+                </div>
+            </form>
+<br>
             <hr>
             <div class="search">
                 <table class="table-fill" id="assign-tasks-table" style="float: left;display: none">
                     <thead>
                     <tr>
+
                         <th class="text-left">Task ID</th>
                         <th class="text-left">Task Name</th>
-                        <th class="text-left">Score</th>
+                        <th class="text-left">Time</th>
+                        <th class="text-left">Delivered</th>
+                        <th class="text-left">Evaluation</th>
+                        <th class="text-left">Delivered On</th>
+                        <th class="text-left">Action</th>
                     </tr>
                     </thead>
                     <tbody class="table-hover">
@@ -139,12 +144,29 @@ session_start();
                         <td class="text-left">2</td>
                         <td class="text-left">ts</td>
                         <td class="text-left">5</td>
+                        <td class="text-left">2 mar</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">3 mar</td>
+                        <td>
+                            <div class="btn-group btn-group-xs">
+                                <button type="button" class="btn" data-id='2'  data-toggle="modal" data-target="#add_score_modal"  style="padding: 10px;"><span class="glyphicon glyphicon-star">Add Task Score</span></button>
+                            </div>
+                        </td>
+
 
                     </tr>
                     <tr>
-                        <td class="text-left">3</td>
-                        <td class="text-left">t3s</td>
-                        <td class="text-left">33</td>
+                        <td class="text-left">2</td>
+                        <td class="text-left">task2</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">2 mar</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">3 mar</td>
+                        <td>
+                            <div class="btn-group btn-group-xs" ng-show="row == $index">
+                                <button type="button" class="btn" data-id='2'  data-toggle="modal" data-target="#add_score_modal"  style="padding: 10px;"><span class="glyphicon glyphicon-star">Add Task Score</span></button>
+                            </div>
+                        </td>
 
                     </tr>
                     </tbody>
@@ -152,9 +174,14 @@ session_start();
                 <table class="table-fill" id="assign-students-table" style="float: left;display: none">
                     <thead>
                     <tr>
+
                         <th class="text-left">Student ID</th>
                         <th class="text-left">Student Name</th>
-                        <th class="text-left">Score</th>
+                        <th class="text-left">Time</th>
+                        <th class="text-left">Delivered</th>
+                        <th class="text-left">Evaluation</th>
+                        <th class="text-left">Delivered On</th>
+                        <th class="text-left">Action</th>
                     </tr>
                     </thead>
                     <tbody class="table-hover">
@@ -162,30 +189,127 @@ session_start();
                         <td class="text-left">2</td>
                         <td class="text-left">ts</td>
                         <td class="text-left">5</td>
+                        <td class="text-left">2 mar</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">3 mar</td>
+                        <td>
+                            <div class="btn-group btn-group-xs">
+                                <button type="button" class="btn" data-id='2'  data-toggle="modal" data-target="#add_score_modal"  style="padding: 10px;"><span class="glyphicon glyphicon-star">Add Task Score</span></button>
+                            </div>
+                        </td>
+
 
                     </tr>
                     <tr>
-                        <td class="text-left">3</td>
-                        <td class="text-left">t3s</td>
-                        <td class="text-left">33</td>
+                        <td class="text-left">2</td>
+                        <td class="text-left">task2</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">2 mar</td>
+                        <td class="text-left">5</td>
+                        <td class="text-left">3 mar</td>
+                        <td>
+                            <div class="btn-group btn-group-xs">
+                                <button type="button" class="btn" data-id='2'  data-toggle="modal" data-target="#add_score_modal"  style="padding: 10px;"><span class="glyphicon glyphicon-star">Add Task Score</span></button>
+                            </div>
+                        </td>
 
                     </tr>
                     </tbody>
                 </table>
-             <div style="margin-left: 560px">
-                 <div class="checkbox">
-                     <label><input type="checkbox" value="">Option 1</label>
-                 </div>
-                 <div class="checkbox">
-                     <label><input type="checkbox" value="">Option 2</label>
-                 </div>
-                 <div class="checkbox disabled">
-                     <label><input type="checkbox" value="" disabled>Option 3</label>
-                 </div>
-             </div>
+
             </div>
 
+            <div class="modal fade" tabindex="-1" role="dialog" id="assign_new_task_modal">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Assign new Task</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="add_new_task_for_student_form" action="../controller.php" method="REQUEST" method="post" >
+                                <h4 style="color: #5d7efe">Choose task/s you want to assign to this student:</h4>
+                                <div class="input-group">
+                                    <select name="assignees" id="assigned_tasks" multiple style="width: 300px">
+                                        <option value="volvo">task1</option>
+                                        <option value="saab">task2</option>
+                                        <option value="opel">task2</option>
+                                        <option value="audi">task3</option>
+                                    </select>
+                                </div>
+                            </form>
 
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" onclick="atLeastChooseOneTask()">Assign</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+            <div class="modal fade" tabindex="-1" role="dialog" id="assign_new_student_modal">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Add New Student</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="add_new_student_for_task_form" action="../controller.php" method="REQUEST" method="post" >
+                                <h4 style="color: #5d7efe">Choose student/s to assign this task for him/them:</h4>
+                                <div class="input-group">
+                                    <select name="assignees" id="students_to_assign_task" multiple style="width: 300px">
+                                        <option value="volvo">yara</option>
+                                        <option value="saab">shurooq</option>
+                                        <option value="opel">fatima</option>
+                                    </select>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" onclick="atLeastChooseOneStudent()">Add</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+            <div class="modal fade" id="add_score_modal" tabindex="-1" role="dialog"  aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Task Evaluation</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Task Name:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Student Name:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Task Score:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="message-text" class="col-form-label">Feedback:</label>
+                                    <textarea class="form-control" id="message-text"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
 
